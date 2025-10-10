a264f623036ebc4ecbd2a6580ce362261a76a751
Bethany Pietroniro <bethany.pietroniro28@gmail.com>
2025-10-10 16:57:09 -0400

    initial models

--- internal/models/jar.go
+++ internal/models/jar.go
@@ -1,1 +1,20 @@
 package models
+
+import "time"
+
+type Jar struct {
+	ID        string
+	CreatedAt time.Time
+	Requests  []Request
+}
+
+type Request struct {
+	ID        string
+	CreatedAt time.Time
+	Method    string
+	Path      string
+	Headers   map[string]string
+	ClientIP  string
+	Body      []byte
+	Query     map[string]string
+}
